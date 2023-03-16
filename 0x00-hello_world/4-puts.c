9f007d582c2825b22246215d852223ccb69b4137
Simon Wanjau <swanjau12@gmail.com>
2023-03-16 15:27:57 -0700

    print using the puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,5 @@
-#include (stdio.h)
+#include <stdio.h>
+
 /**
  * main - this is the main function
  * Return: it is 0
@@ -7,6 +8,6 @@
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle);
+	puts("\"Programming is like building a multilingual puzzle");
         return(0);
 }
