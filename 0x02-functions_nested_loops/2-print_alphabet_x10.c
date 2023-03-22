28b4090e7adef9a6a24df80ca6195d58418e0bd9
Simon Wanjau <swanjau12@gmail.com>
2023-03-22 10:53:01 -0700

    prints 10* the alphabets

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - function that prints 10* alphabets
+ * Return: is zero
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char j;
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		       	_putchar(j);
+	        _putchar('\n');	
+	}
+}
