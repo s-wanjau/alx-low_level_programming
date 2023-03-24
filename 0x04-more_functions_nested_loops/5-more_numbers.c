4103c391e2bd3566d4ccde928e48e94694d893c0
Simon Wanjau <swanjau12@gmail.com>
2023-03-24 14:34:04 -0700

    print 10* 0-14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10* numbers 1-14
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int x, y;
+
+	for (x = 1; x <= 10; x++)
+	{
+		for (y = 0; y <= 14; y++)
+		{
+			if (y >= 10)
+			_putchar('1');
+			_putchar(y % 10 + '0');
+		}
+			_putchar('\n');
+		}
+}
+
