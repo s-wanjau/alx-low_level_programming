49bc41b5df52cd77d6b079777f46bc9c72ca991f
Simon Wanjau <swanjau12@gmail.com>
2023-03-24 11:34:55 -0700

    print 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers 0 - 9
+ *
+ * Return: is void
+ */
+
+void print_numbers(void)
+{
+	char b;
+	for (b = '0'; b <= '9'; b++)
+	{
+		_putchar(b);
+	}
+	_putchar('\n');
+
+
+}
