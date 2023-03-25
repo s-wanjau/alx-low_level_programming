e44fdc8e74c2bbf843adc32c0beb0d2a38ac7c50
Simon Wanjau <swanjau12@gmail.com>
2023-03-25 15:20:23 -0700

    print remaining days in a leap year

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -12,4 +12,7 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
+int main(void);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
 #endif
