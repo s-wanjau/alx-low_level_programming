a1dd7b48cadadebc9c1b0ea39139dd05f283a3a8
Simon Wanjau <swanjau12@gmail.com>
2023-03-26 14:42:16 -0700

    print 10* alphabet lowercase

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,10 +8,13 @@ void print_alphabet_x10(void)
 {
 	int i;
 	char j;
+
 	for (i = 1; i <= 10; i++)
 	{
 		for (j = 'a'; j <= 'z'; j++)
-		       	_putchar(j);
-	        _putchar('\n');	
+
+			_putchar(j);
+
+		_putchar('\n');	
 	}
 }
