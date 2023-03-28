1c43ec9550afd77b132826b0a15bfeebd772f932
Simon Wanjau <swanjau12@gmail.com>
2023-03-28 02:06:46 -0700

    print lowercase alphabets except q and e

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * return: is zero
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	   putchar(i); 
+        putchar("\n");
+	return (0);
+}
